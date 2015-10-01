9344cfe47ffa88119a963d50e79c0480d2e28ab4
reglass2 <novikov.grigoriy@gmail.com>
2015-10-01 23:34:45 +0300

    .

---
+++ test/assets/js/main.js
@@ -0,0 +1,145 @@
+/* skel-baseline v3.0.0-dev | (c) n33 | skel.io | MIT licensed */
+
+(function() {
+
+	"use strict";
+
+	// Methods/polyfills.
+
+		// addEventsListener
+			var addEventsListener=function(o,t,e){var n,i=t.split(" ");for(n in i)o.addEventListener(i[n],e)}
+
+		// classList | (c) @remy | github.com/remy/polyfills | rem.mit-license.org
+			!function(){function t(t){this.el=t;for(var n=t.className.replace(/^\s+|\s+$/g,"").split(/\s+/),i=0;i<n.length;i++)e.call(this,n[i])}function n(t,n,i){Object.defineProperty?Object.defineProperty(t,n,{get:i}):t.__defineGetter__(n,i)}if(!("undefined"==typeof window.Element||"classList"in document.documentElement)){var i=Array.prototype,e=i.push,s=i.splice,o=i.join;t.prototype={add:function(t){this.contains(t)||(e.call(this,t),this.el.className=this.toString())},contains:function(t){return-1!=this.el.className.indexOf(t)},item:function(t){return this[t]||null},remove:function(t){if(this.contains(t)){for(var n=0;n<this.length&&this[n]!=t;n++);s.call(this,n,1),this.el.className=this.toString()}},toString:function(){return o.call(this," ")},toggle:function(t){return this.contains(t)?this.remove(t):this.add(t),this.contains(t)}},window.DOMTokenList=t,n(Element.prototype,"classList",function(){return new t(this)})}}();
+
+	// Vars.
+		var	$body = document.querySelector('body'),
+			$header = $('#header'),
+			$window = $(window),
+			$banner = $('#banner');
+
+	// Breakpoints.
+		skel.breakpoints({
+			xlarge:	'(max-width: 1680px)',
+			large:	'(max-width: 1280px)',
+			medium:	'(max-width: 980px)',
+			small:	'(max-width: 770px)',
+			xsmall:	'(max-width: 480px)'
+		});
+		
+		skel.layout({
+			reset: "normalize",
+			grid: true,
+			containers: true,
+			conditionals: true
+		});
+
+	// Disable animations/transitions until everything's loaded.
+		$body.classList.add('is-loading');
+
+		window.addEventListener('load', function() {
+			$body.classList.remove('is-loading');
+		});
+
+	// Nav.
+		var	$nav = document.querySelector('#nav'),
+			$navToggle = document.querySelector('a[href="#nav"]'),
+			$navClose;
+
+		// Event: Prevent clicks/taps inside the nav from bubbling.
+			addEventsListener($nav, 'click touchend', function(event) {
+				event.stopPropagation();
+			});
+
+		// Event: Hide nav on body click/tap.
+			addEventsListener($body, 'click touchend', function(event) {
+				$nav.classList.remove('visible');
+			});
+			
+		// Dropdowns.
+			$('#main-nav > ul').dropotron({
+				mode: 'fade',
+				baseZIndex: 10005,
+				hideDelay: 450,
+				mode: 'fade',
+				alignment: 'center',
+				noOpenerFade: true,
+				expandMode: (skel.vars.isTouch ? 'click' : 'hover')
+			});
+
+		// Toggle.
+
+			// Event: Toggle nav on click.
+				$navToggle.addEventListener('click', function(event) {
+
+					event.preventDefault();
+					event.stopPropagation();
+
+					$nav.classList.toggle('visible');
+
+				});
+
+		// Close.
+
+			// Create element.
+				$navClose = document.createElement('a');
+					$navClose.href = '#';
+					$navClose.className = 'close';
+					$navClose.tabIndex = 0;
+					$nav.appendChild($navClose);
+
+			// Event: Hide on ESC.
+				window.addEventListener('keydown', function(event) {
+
+					if (event.keyCode == 27)
+						$nav.classList.remove('visible');
+
+				});
+
+			// Event: Hide nav on click.
+				$navClose.addEventListener('click', function(event) {
+
+					event.preventDefault();
+					event.stopPropagation();
+
+					$nav.classList.remove('visible');
+
+				});
+				
+		// Header.
+		// If the header is using "alt" styling and #banner is present, use scrollwatch
+		// to revert it back to normal styling once the user scrolls past the banner.
+		// Note: This is disabled on mobile devices.
+			// if (!skel.vars.isMobile
+			// &&	$header.hasClass('alt')
+			// &&	$banner.length > 0) {
+
+				// $window.on('load', function() {
+
+					// $banner.scrollwatch({
+						// delay:		0,
+						// range:		1,
+						// anchor:		'top',
+						// on:			function() { $header.addClass('alt reveal'); },
+						// off:		function() { $header.removeClass('alt'); }
+					// });
+
+				// });
+
+			// }
+			if (!skel.vars.mobile	&&	$header.hasClass('alt') && !skel.breakpoint("small").active){
+
+				$window.on('load', function() {
+
+					$banner.scrollwatch({
+						delay:		0,
+						range:		1,
+						anchor:		'top',
+						on:			function() { $header.addClass('alt reveal'); },
+						off:		function() { $header.removeClass('alt'); }
+					});
+
+				});
+
+			}
+})(jQuery);
