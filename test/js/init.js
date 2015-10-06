3441948ae0a013baf6ca5956fc0e576b70020e45
reglass2 <novikov.grigoriy@gmail.com>
2015-10-06 21:19:58 +0300

    .

--- test/js/init.js
+++ test/js/init.js
@@ -1,151 +1,111 @@
 
-(function() {
+(function($) {
 
-	"use strict";
-
-	// Methods/polyfills.
-
-		// addEventsListener
-		var addEventsListener=function(o,t,e){var n,i=t.split(" ");for(n in i)o.addEventListener(i[n],e)}
-
-		// classList | (c) @remy | github.com/remy/polyfills | rem.mit-license.org
-		!function(){function t(t){this.el=t;for(var n=t.className.replace(/^\s+|\s+$/g,"").split(/\s+/),i=0;i<n.length;i++)e.call(this,n[i])}function n(t,n,i){Object.defineProperty?Object.defineProperty(t,n,{get:i}):t.__defineGetter__(n,i)}if(!("undefined"==typeof window.Element||"classList"in document.documentElement)){var i=Array.prototype,e=i.push,s=i.splice,o=i.join;t.prototype={add:function(t){this.contains(t)||(e.call(this,t),this.el.className=this.toString())},contains:function(t){return-1!=this.el.className.indexOf(t)},item:function(t){return this[t]||null},remove:function(t){if(this.contains(t)){for(var n=0;n<this.length&&this[n]!=t;n++);s.call(this,n,1),this.el.className=this.toString()}},toString:function(){return o.call(this," ")},toggle:function(t){return this.contains(t)?this.remove(t):this.add(t),this.contains(t)}},window.DOMTokenList=t,n(Element.prototype,"classList",function(){return new t(this)})}}();
-
-	// Vars.
-		var	$body = document.querySelector('body'),
-			$header = $('#header'),
-			$window = $(window),
-			$banner = $('#banner');
-
-	// Breakpoints.
-		skel.breakpoints({
+	skel
+		.breakpoints({
 			xlarge:	'(max-width: 1680px)',
 			large:	'(max-width: 1280px)',
 			medium:	'(max-width: 980px)',
-			small:	'(max-width: 770px)',
+			small:	'(max-width: 736px)',
 			xsmall:	'(max-width: 480px)'
 		});
-		
-		skel.layout({
-			reset: "normalize",
-			grid: true,
-			containers: '100%',
-			conditionals: true
-		});
-		
-		skel.viewport({
-			width: "device-width",
-			scalable: true,
-			breakpoints: {
-				medium: {
-					containers: "90%",
-					width: "device-width"
-				},
-				small: {
-					scalable: false
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$wrapper = $('#page-wrapper'),
+			$banner = $('#banner'),
+			$header = $('#header');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+			skel.viewport({
+				width: "device-width",
+				scalable: true,
+				breakpoints: {
+					small: {
+						scalable: false
+					}
 				}
-			}
-		});
-
-	// Disable animations/transitions until everything's loaded.
-		$body.classList.add('is-loading');
-
-		window.addEventListener('load', function() {
-			$body.classList.remove('is-loading');
-		});
-		
-	//Owl carousel
-		$(document).ready(function(){
-		  $(".owl-carousel").owlCarousel({
-			  items: 1,
-			  loop: true,
-			  nav: false
-			  
-		  });
-		});
-		
-		$(document).ready(function(){
-		  $('.slick-slider').slick({
-			dots: true
-		  });
-		});
-	
-	// Nav.
-		var	$nav = document.querySelector('#nav'),
-			$navToggle = document.querySelector('a[href="#nav"]'),
-			$navClose;
-
-		// Event: Prevent clicks/taps inside the nav from bubbling.
-			addEventsListener($nav, 'click touchend', function(event) {
-				event.stopPropagation();
-			});
-
-		// Event: Hide nav on body click/tap.
-			addEventsListener($body, 'click touchend', function(event) {
-				$nav.classList.remove('visible');
 			});
 			
+			skel.layout({
+				reset: "full",
+				grid: true
+			});
 		
-		// Toggle.
-
-			// Event: Toggle nav on click.
-				$navToggle.addEventListener('click', function(event) {
-
-					event.preventDefault();
-					event.stopPropagation();
-
-					$nav.classList.toggle('visible');
-
-				});
-
-		// Close.
-
-			// Create element.
-				$navClose = document.createElement('a');
-					$navClose.href = '#';
-					$navClose.className = 'close';
-					$navClose.tabIndex = 0;
-					$nav.appendChild($navClose);
-
-			// Event: Hide on ESC.
-				window.addEventListener('keydown', function(event) {
-
-					if (event.keyCode == 27)
-						$nav.classList.remove('visible');
-
-				});
-
-			// Event: Hide nav on click.
-				$navClose.addEventListener('click', function(event) {
-
-					event.preventDefault();
-					event.stopPropagation();
-
-					$nav.classList.remove('visible');
-
-				});
-			
-		// Header.
-		// If the header is using "alt" styling and #banner is present, use scrollwatch
-		// to revert it back to normal styling once the user scrolls past the banner.
-		// Note: This is disabled on mobile devices.
-			if (!skel.vars.mobile
-			&&	!skel.breakpoint("small").active
-			&&	$header.hasClass('alt')
-			&&	$banner.length > 0) {
-
-				$window.on('load', function() {
-
-					$banner.scrollwatch({
-						delay:		0,
-						range:		1,
-						anchor:		'top',
-						on:			function() { $header.addClass('alt'); },
-						off:		function() { $header.removeClass('alt'); }
+		// Mobile?
+			if (skel.vars.mobile)
+				$body.addClass('is-mobile');
+			else
+				skel
+					.on('-medium !medium', function() {
+						$body.removeClass('is-mobile');
+					})
+					.on('+medium', function() {
+						$body.addClass('is-mobile');
 					});
 
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Scrolly.
+			$('.scrolly')
+				.scrolly({
+					speed: 1500,
+					offset: $header.outerHeight()
+				});
+
+		// Menu.
+			$('#menu')
+				.append('<a href="#menu" class="close"></a>')
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right',
+					target: $body,
+					visibleClass: 'is-menu-visible'
+				});
+
+		// Header.
+			if (skel.vars.IEVersion < 9)
+				$header.removeClass('alt');
+
+			if ($banner.length > 0
+			&&	$header.hasClass('alt')) {
+
+				$window.on('resize', function() { $window.trigger('scroll'); });
+
+				$banner.scrollex({
+					bottom:		$header.outerHeight() + 1,
+					terminate:	function() { $header.removeClass('alt'); },
+					enter:		function() { $header.addClass('alt'); },
+					leave:		function() { $header.removeClass('alt'); }
 				});
 
 			}
-			
+
+	});
+
 })(jQuery);
+
