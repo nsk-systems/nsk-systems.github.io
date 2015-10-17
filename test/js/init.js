8e184abc6cfea7c107f89b6cd63e07fe059ff141
reglass2 <novikov.grigoriy@gmail.com>
2015-10-17 16:29:42 +0300

    .

--- test/js/init.js
+++ test/js/init.js
@@ -4,8 +4,8 @@
 	skel
 		.breakpoints({
 			xlarge:	'(max-width: 1680px)',
-			// large:	'(max-width: 1280px)',
-			// medium:	'(max-width: 980px)',
+			large:	'(max-width: 1280px)',
+			medium:	'(max-width: 980px)',
 			small:	'(max-width: 736px)',
 			xsmall:	'(max-width: 480px)',
 			mobile:	'(max-width: 320px)'
