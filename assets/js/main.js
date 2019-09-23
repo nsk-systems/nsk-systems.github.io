da7c5c6977fab82eba22fbf60c2cb9228f4f8ab2
reglass <reglass@users.noreply.github.com>
2019-09-23 13:30:10 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,7 @@
 				var form = $(this);
 				$.ajax({
 					url: "https://script.google.com/macros/s/AKfycbzP4Fpk9dyYqfKNluJ2RJHDjKxZX0KtPFd8nqfaSg5z5PjXr2oI/exec",
+					method: "POST",
 					data: form.serialize()
 						}).done(function(data) {
 							$(".ajax-form").hide();
