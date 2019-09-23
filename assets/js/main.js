9d858359e5fed417e56a721cf864b69e87ab6dbc
reglass <reglass@users.noreply.github.com>
2019-09-23 13:26:11 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,8 +18,7 @@
 				event.preventDefault();
 				var form = $(this);
 				$.ajax({
-					dataType: "json",
-					url: "https://script.google.com/macros/s/AKfycbzP4Fpk9dyYqfKNluJ2RJHDjKxZX0KtPFd8nqfaSg5z5PjXr2oI/exec", //"https://getsimpleform.com/messages/ajax?form_api_token=6155e5a53e9a2468cc242519b0569ead",
+					url: "https://script.google.com/macros/s/AKfycbzP4Fpk9dyYqfKNluJ2RJHDjKxZX0KtPFd8nqfaSg5z5PjXr2oI/exec",
 					data: form.serialize()
 						}).done(function(data) {
 							$(".ajax-form").hide();
